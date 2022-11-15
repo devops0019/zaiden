aa6479e9f857f5b533d58be110e9ca4bb1b9200a
Gauravp15 <gauravprabhu15@gmail.com>
2022-11-15 19:13:59 +0530

    Added new files

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -9,7 +9,7 @@ export const Footer = ({setCommonFormState}) => {
                         <div className="footer-left">
                             <p>Contact: <a href="tel:+917507070707">7507070707</a></p>
                             <p>Email: <a href="mailto:info@roswalt.com">info@roswalt.com</a></p>
-                            <p>Address: 903, Naman Centre, G Block, Bandra Kurla Complex, Mumbai - 400051.</p>
+                            <p>Address: A S Hightech, 16, Koteshwar Palace CHSL, Jeeva Mahale Road, Andheri (East), Mumbai - 400069.</p>
                         </div>
                         <div className="footer-right">
                             <button type="button" onClick={() => {setCommonFormState(true)}}><span><img src="images/download.svg" alt="Download brochure" /></span><span className="mobile-hide">Download brochure</span></button>
@@ -17,16 +17,26 @@ export const Footer = ({setCommonFormState}) => {
                     </div>
                     <div className="footer-lower-wrapper">
                         <div className="footer-copy-wrapper">
-                            <p>&copy; {new Date().getFullYear()} Roswalt Realty</p>
+                            <p>&copy; {new Date().getFullYear()} ROSWALT REALTY PVT. LTD.</p>
                             <ul className="social-icons-list">
-                                <li>
+                                <li className="facebook">
                                     <a href="https://www.facebook.com/roswaltrealty" rel="noreferrer" target="_blank">
                                         <img src="images/facebook.svg" alt="Facebook" />
                                     </a>
                                 </li>
-                                <li>
+                                <li className="instagram">
                                     <a href="https://www.instagram.com/roswaltrealty_mumbai" rel="noreferrer" target="_blank">
                                         <img src="images/instagram.svg" alt="Instagram" />
+                                    </a>
+                                </li>
+                                <li className="linkedin">
+                                    <a href="https://www.linkedin.com/company/28707255/" rel="noreferrer" target="_blank">
+                                        <img src="images/linkedin.svg" alt="LinkedIn" />
+                                    </a>
+                                </li>
+                                <li className="youtube">
+                                    <a href="https://www.youtube.com/channel/UCE5zUCQ5KCAf9LzLJYDWIeg" rel="noreferrer" target="_blank">
+                                        <img src="images/youtube.svg" alt="Youtube" />
                                     </a>
                                 </li>
                             </ul>
